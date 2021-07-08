b7a67b6542deb79191d1b627e12311c877a8f7eb
songhaiyu <songhaiyu@smartlink-tech.com.cn>
2021-07-09 02:15:03 +0800

    mybatis-plus-generator

---
+++ sc-mybatis-plus-generator/src/main/java/com/xiaoxialemi/scmybatisplus/ScMybatisPlusGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoxialemi.scmybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScMybatisPlusGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScMybatisPlusGeneratorApplication.class, args);
+    }
+
+}
